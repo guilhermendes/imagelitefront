4938be0b67d7abee7cc84905827d893d5c4c1528
guiweb85@gmail.com <guiweb85@gmail.com>
2023-12-05 21:19:10 -0300

    36. Conhecendo o React Hook useState

--- src/app/galeria/page.tsx
+++ src/app/galeria/page.tsx
@@ -1,16 +1,43 @@
 
+'use client'
+
 import { Template, ImageCard } from '@/components'
+import { useState } from 'react'
 
 export default function GaleriaPage(){
+
+    const image1 = 'https://cdn6.campograndenews.com.br/uploads/noticias/2023/05/08/6458dbeeb7683.jpg';
+    const image2 = 'https://cf.bstatic.com/xdata/images/hotel/max1024x768/322625648.jpg?k=c880dc259724628f33c2272282051f27f0fb175b79a5dd6f0f015727dcd32d8b&o=&hp=1';
+    const nome1 = 'Paraiso'
+    const nome2 = 'Natureza'
+
+
+    const [codigoImage, setCodigoImage] = useState<number>(2);
+    const [urlImagem, setUrlImagem] = useState<string>(image2);
+    const [nomeImage, setNomeImage] = useState<string>(nome2)
+
+    
+    function mudarImage(){
+        if(codigoImage == 1){
+            setCodigoImage(2)
+            setUrlImagem(image2)
+            setNomeImage(nome2)
+
+        }else{
+            setCodigoImage(1)
+            setUrlImagem(image1)
+            setNomeImage(nome1)
+        }
+
+    }
+
     return (
         <Template>
+            <button className='bg-gray-500' onClick={mudarImage}>Clique para mudar</button>
             <section className='grid grid-cols-4 gap-8' >
-                <ImageCard nome="lorem ipsum" tamanho="10mb" dataUpload='01/01/2024' src="https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA1L2ZsMjMxNDY4ODE2NTQtaW1hZ2Uta295NWoyeG4uanBn.jpg"/>
-                <ImageCard nome="lorem ipsum"src="https://images.unsplash.com/photo-1546587348-d12660c30c50?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8bmF0dXJhbHxlbnwwfHwwfHx8MA%3D%3D"/>
-                <ImageCard nome="lorem ipsum"src="https://img.freepik.com/free-photo/shiraito-waterfall-autumn-japan_335224-193.jpg"/>
-                <ImageCard nome="lorem ipsum"src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcjb37k7YjRZZzndIiwtRVUw7YBDXCAVZtfQ&usqp=CAU"/>
-                <ImageCard nome="lorem ipsum"src="https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg"/>
-                <ImageCard nome="lorem ipsum" src="https://t3.ftcdn.net/jpg/05/35/47/38/360_F_535473874_OWCa2ohzXXNZgqnlzF9QETsnbrSO9pFS.jpg"/>
+                <ImageCard nome={nomeImage} tamanho="10mb" dataUpload='01/01/2024' src={ urlImagem }/>
+                <ImageCard nome={nomeImage} tamanho="10mb" dataUpload='01/01/2024' src={ urlImagem }/>
+                <ImageCard nome={nomeImage} tamanho="10mb" dataUpload='01/01/2024' src={ urlImagem }/>
             </section>
         </Template>
     )
